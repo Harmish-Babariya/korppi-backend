ea2475d291a2774b92230d5893dd09e304e6e73b
Harmish Babariya <harmishbabariya@gmail.com>
2023-12-23 18:38:32 +0530

    Feat: changed project name in api

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,14 +2,14 @@ const authRoutes = require('./auth')
 module.exports = (app) => {
   app.get("/", (req, res) => {
     res.json({
-      message: "These are Probulon APIs",
+      message: "These are Korppi APIs",
       api_health: "good",
       api_version: "V1.0.0",
     });
   });
   app.get("/hello", (req, res) => {
     res.json({
-      message: "These are Probulon APIs",
+      message: "These are Korppi APIs",
       api_health: "good",
       api_version: "V1.0.0",
     });
