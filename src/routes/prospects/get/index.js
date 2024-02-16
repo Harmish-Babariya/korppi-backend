29be17de5b95a519981f02d843626ffdc35e690a
denishababariya <denishababariya899@gmail.com>
2024-02-16 15:03:28 +0530

    changed string to array and mongo query

--- src/routes/prospects/get/index.js
+++ src/routes/prospects/get/index.js
@@ -17,58 +17,82 @@ const makeMongoDbServiceCompany = require("../../../services/db/dbService")({
 exports.handler = async (req, res) => {
   try {
     let industryId = await makeMongoDbServiceIndustry.getSingleDocumentByQuery(
-      { name: { $regex: '.*' + req.body.industry + '.*', $options: 'i' } },
-      ['_id']
-    )
+      { name: { $in: req.body.industry } },
+      ["_id"]
+    );
     
     let companies = await makeMongoDbServiceCompany.getDocumentByQuery(
-    { $or: [
-      { size: { $gt: req.body.employeeCount }},
-      { coutry: { $regex: '.*' + req.body.location + '.*', $options: 'i' } },
-      { industryId }
-    ]},
-      ['_id']
-    )
-
-    companies = companies.map(ele => ele._id)
+      {
+        size: { $in: req.body.employeeCount },
+        country: { $in: req.body.location },
+        industryId,
+      },
+      ["_id"]
+    );
+    
+    companies = companies.map((ele) => ele._id);
+    console.log(companies)
     let meta = {};
     let prospectsList = [];
     const pageNumber = parseInt(req.body.pageNumber);
     const pageSize = parseInt(req.body.pageSize);
     const skip = pageNumber === 1 ? 0 : parseInt((pageNumber - 1) * pageSize);
-    const matchQuery = { 
-      company : { $in: companies },
-      role: { $regex: req.body.role, $options: 'i' }
-  };
-    if (req.body.search && typeof req.body.search !== "undefined" && req.body.search !== '') {
+    const matchQuery = {
+      company: { $in: companies },
+      role: { $regex: req.body.role, $options: "i" },
+    };
+    if (
+      req.body.search &&
+      typeof req.body.search !== "undefined" &&
+      req.body.search !== ""
+    ) {
       matchQuery.$or = [
-        { firstName: { $regex: '.*' + req.body.search + '.*', $options: 'i' } },
-        { lastName: { $regex: '.*' + req.body.search + '.*', $options: 'i' } },
-        { email: { $regex: '.*' + req.body.search + '.*', $options: 'i' } },
-        { role: { $regex: '.*' + req.body.search + '.*', $options: 'i' } },
-        { company: { $regex: '.*' + req.body.search + '.*', $options: 'i' } },
-        { phone: { $regex: '.*' + req.body.search + '.*', $options: 'i' } },
+        { firstName: { $regex: ".*" + req.body.search + ".*", $options: "i" } },
+        { lastName: { $regex: ".*" + req.body.search + ".*", $options: "i" } },
+        { email: { $regex: ".*" + req.body.search + ".*", $options: "i" } },
+        { role: { $regex: ".*" + req.body.search + ".*", $options: "i" } },
+        { company: { $regex: ".*" + req.body.search + ".*", $options: "i" } },
+        { phone: { $regex: ".*" + req.body.search + ".*", $options: "i" } },
       ];
     }
-    prospectsList = await makeMongoDbService.getDocumentByCustomAggregation([
-      {
-          $match: matchQuery
-      },
-      { $sort: { _id: -1 } },
-      { $skip: skip },
-      { $limit: pageSize },
-  ])
-  const prospectsCount = await makeMongoDbService.getCountDocumentByQuery(matchQuery);
-  meta = {
-    pageNumber,
-    pageSize,
-    totalCount: prospectsCount,
-    prevPage: parseInt(pageNumber) === 1 ? false : true,
-    nextPage: parseInt(prospectsCount) / parseInt(pageSize) <= parseInt(pageNumber) ? false : true,
-    totalPages: Math.ceil(parseInt(prospectsCount) / parseInt(pageSize)),
-  };
-    return sendResponse(res,null,200,messages.successResponse(prospectsList, meta));
+    prospectsList = await makeMongoDbService.getDocumentByQueryPopulate(
+      matchQuery,
+      ["_id", "firstName", "lastName", "company"],
+      ["company"],
+      pageNumber,
+      pageSize,
+      { _id: -1 }
+    );
+    //   prospectsList = await makeMongoDbService.getDocumentByCustomAggregation([
+    //     {
+    //         $match: matchQuery
+    //     },
+    //     { $sort: { _id: -1 } },
+    //     { $skip: skip },
+    //     { $limit: pageSize },
+    // ])
+    const prospectsCount = await makeMongoDbService.getCountDocumentByQuery(
+      matchQuery
+    );
+    meta = {
+      pageNumber,
+      pageSize,
+      totalCount: prospectsCount,
+      prevPage: parseInt(pageNumber) === 1 ? false : true,
+      nextPage:
+        parseInt(prospectsCount) / parseInt(pageSize) <= parseInt(pageNumber)
+          ? false
+          : true,
+      totalPages: Math.ceil(parseInt(prospectsCount) / parseInt(pageSize)),
+    };
+    return sendResponse(
+      res,
+      null,
+      200,
+      messages.successResponse(prospectsList, meta)
+    );
   } catch (error) {
+    console.log(error);
     return sendResponse(res, null, 500, messages.failureResponse());
   }
 };
@@ -77,8 +101,8 @@ exports.rule = Joi.object({
   pageNumber: Joi.number().optional().default(1).description("PageNumber"),
   pageSize: Joi.number().optional().default(20).description("PageNumber"),
   search: Joi.string().optional().description("search"),
-  employeeCount: Joi.string().required().description('employeeCount').example('employeeCount'),
-  location: Joi.string().required().description('location').example('location'),
-  industry: Joi.string().required().description('industry').example('industry'),
-  role: Joi.string().required().description('role').example('CEO'),
+  employeeCount: Joi.array().required().description("employeeCount"),
+  location: Joi.array().required().description("location"),
+  industry: Joi.array().required().description("industry"),
+  role: Joi.string().required().description("role").example("CEO"),
 });
