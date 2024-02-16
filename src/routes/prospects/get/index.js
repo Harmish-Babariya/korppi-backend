7b92c2ee9fd917f7826ac9c26ba6b98739df31a1
denishababariya <denishababariya899@gmail.com>
2024-02-16 14:33:42 +0530

    feat(prospect): get prospect api

--- src/routes/prospects/get/index.js
+++ src/routes/prospects/get/index.js
@@ -1,18 +1,45 @@
 const { sendResponse, messages } = require("../../../helpers/handleResponse");
+const { ObjectId } = require("mongodb");
 const Joi = require("joi");
 const { Prospects } = require("../../../models/prospects.model");
 const makeMongoDbService = require("../../../services/db/dbService")({
   model: Prospects,
 });
+const { Industry } = require("../../../models/industry.model");
+const makeMongoDbServiceIndustry = require("../../../services/db/dbService")({
+  model: Industry,
+});
+const { Company } = require("../../../models/company.model");
+const makeMongoDbServiceCompany = require("../../../services/db/dbService")({
+  model: Company,
+});
 
 exports.handler = async (req, res) => {
   try {
+    let industryId = await makeMongoDbServiceIndustry.getSingleDocumentByQuery(
+      { name: { $regex: '.*' + req.body.industry + '.*', $options: 'i' } },
+      ['_id']
+    )
+    
+    let companies = await makeMongoDbServiceCompany.getDocumentByQuery(
+    { $or: [
+      { size: { $gt: req.body.employeeCount }},
+      { coutry: { $regex: '.*' + req.body.location + '.*', $options: 'i' } },
+      { industryId }
+    ]},
+      ['_id']
+    )
+
+    companies = companies.map(ele => ele._id)
     let meta = {};
     let prospectsList = [];
     const pageNumber = parseInt(req.body.pageNumber);
     const pageSize = parseInt(req.body.pageSize);
     const skip = pageNumber === 1 ? 0 : parseInt((pageNumber - 1) * pageSize);
-    const matchQuery = { };
+    const matchQuery = { 
+      company : { $in: companies },
+      role: { $regex: req.body.role, $options: 'i' }
+  };
     if (req.body.search && typeof req.body.search !== "undefined" && req.body.search !== '') {
       matchQuery.$or = [
         { firstName: { $regex: '.*' + req.body.search + '.*', $options: 'i' } },
@@ -49,5 +76,9 @@ exports.handler = async (req, res) => {
 exports.rule = Joi.object({
   pageNumber: Joi.number().optional().default(1).description("PageNumber"),
   pageSize: Joi.number().optional().default(20).description("PageNumber"),
-  search: Joi.string().optional().allow('').description('search').example('john')
+  search: Joi.string().optional().description("search"),
+  employeeCount: Joi.string().required().description('employeeCount').example('employeeCount'),
+  location: Joi.string().required().description('location').example('location'),
+  industry: Joi.string().required().description('industry').example('industry'),
+  role: Joi.string().required().description('role').example('CEO'),
 });
