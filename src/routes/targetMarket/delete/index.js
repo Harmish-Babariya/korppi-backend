8a891fce4d3dbdced92cb91d51703b09f87730f9
Harmish-Babariya <harmishbabariya@gmail.com>
2024-02-28 12:51:52 +0530

    delete target market

--- src/routes/targetMarket/delete/index.js
+++ src/routes/targetMarket/delete/index.js
@@ -12,11 +12,11 @@ const makeMongoDbServiceService = require("../../../services/db/dbService")({
 
 exports.handler = async (req, res) => {
   try {
-    await makeMongoDbService.deleteDocument(req.body.targetMarketId)
-    await makeMongoDbServiceService.updateDocument(req.body.serviceId, {
-      $pull: { target_market: req.body.targetMarketId }
+    await makeMongoDbService.findOneAndDeleteDocument(new ObjectId(req.body.targetMarketId))
+    await makeMongoDbServiceService.updateDocument(new ObjectId(req.body.serviceId), {
+      $pull: { target_market: new ObjectId(req.body.targetMarketId) }
     })
-    return sendResponse(res, null, 200, messages.successResponse(data));
+    return sendResponse(res, null, 200, messages.successResponse());
   } catch (error) {
     return sendResponse(res, null, 500, messages.failureResponse());
   }
