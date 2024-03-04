47e50eb153913fc1b28ee61e966af4ce941db48e
Harmish-Babariya <harmishbabariya@gmail.com>
2024-03-04 17:41:55 +0530

    fix: cors

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 module.exports = function (app) {
     app.use(express.json());
-    app.use(cors({ origin: "*" }));
+    app.use(cors({ origin: true }));
     app.use(bodyParser.json({ limit: "50mb" }));
     app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 }
