f013157c28af662aea7c53e413ca50c1e7cc18e6
Harmish-Babariya <harmishbabariya@gmail.com>
2024-03-16 17:14:02 +0530

    added service id

--- src/routes/email/getCountByUser/index.js
+++ src/routes/email/getCountByUser/index.js
@@ -9,10 +9,11 @@ const makeMongoDbService = require("../../../services/db/dbService")({
 exports.handler = async (req, res) => {
   try {
     const matchQuery = { userId: req.user._id, isSent: false };
+    const service = await makeMongoDbService.getSingleDocumentByQuery(matchQuery, ['service'])
     const emailCount = await makeMongoDbService.getCountDocumentByQuery(
       matchQuery
     );
-    return sendResponse(res, null, 200, messages.successResponse({ emailCount }));
+    return sendResponse(res, null, 200, messages.successResponse({ emailCount, service: service ? service.service : null }));
   } catch (error) {
     return sendResponse(res, null, 500, messages.failureResponse());
   }
